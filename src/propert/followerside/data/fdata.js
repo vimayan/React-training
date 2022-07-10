e21466ba677a1d85e999d61cee024fd911206139
vimayan <joblessdesks@gmail.com>
2022-07-10 14:54:21 +0530

    fourth commit

--- src/propert/followerside/data/fdata.js
+++ src/propert/followerside/data/fdata.js
@@ -3,7 +3,7 @@ import img2 from '../../.././img/img2.png'
 import img3 from '../../.././img/img3.png'
 import img4 from '../../.././img/img4.jpg'
 
-export const followers = [{name:'priya sweety',username:'priya sweety',img:img1},
-{name:'priya sweety',username:'priya sweety',img:img2},
-{name:'priya sweety',username:'priya sweety',img:img3},
-{name:'priya sweety',username:'priya sweety',img:img4} ]
+export const followers = [{name:'priya sweety',username:'little princes',img:img1},
+{name:'arun kumar',username:'king in hell',img:img2},
+{name:'hunger games',username:'almigty the god',img:img3},
+{name:'pinky sweety',username:'mylife myrules',img:img4} ]
